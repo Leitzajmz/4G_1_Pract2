fa727e1288d28b983026ede6d620ba335f1ee0fb
LeitzaJmz <yoshia_98@hotmail.com>
2018-04-07 00:03:14 -0500

    Prueba1

--- PruebaDePractica.X/main.c
+++ PruebaDePractica.X/main.c
@@ -10,7 +10,6 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
-void rutinaDeError();
 
 void main (void){
    setup_oscillator(OSC_16MHZ);
@@ -24,46 +23,6 @@ void main (void){
 
    while(1){
       opcion = (long)input_b();
-         switch(opcion){
-            case 0x10: //Suma
-               resultado = (long)input_c() + (long)input_d();
-               break;
-            case 0x20://Resta
-               if ((long)input_d() > (long)input_c()){
-                 complemento1 = ~(long)input_d();
-                 resultado = complemento1;
-                 /*complemento1 += 1; 
-                  resultado = (long)input_c() + complemento1;*/
-               }
-               else{
-                  resultado = (long)input_c() - (long)input_d();
-               }
-               break; 
-            case 0x40: //Multiplicacion
-               resultado = (long)input_c() * (long)input_d();
-               break;
-            case 0x80: //Division
-                if((long)input_d() == 0)
-                    rutinaDeError();
-                else
-                   resultado = (long)input_c() / (long)input_d();
-               break;
-       }
-            output_a(resultado & 0x3F);
-            output_b(resultado & 0x3C0);
-            output_e(resultado & 0x1C00);      
-   }
-}
-
-void rutinaDeError(){
-    for (int i = 0 ; i < 5 ; i++){
-        output_a(0xFF);
-        output_b(0x0F);
-        output_e(0x07);
-        delay_ms(150);
-        output_a(0x00);
-        output_b(0x00);
-        output_e(0x00);
-        delay_ms(150);
+         
     }
 }
